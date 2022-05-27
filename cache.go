376aedd0c86ddd77e5f209388ea7a05b78a53e7f
alexhaiduk <alexhaiduk82@gmail.com>
2022-05-27 20:09:49 +0300

    start

--- cache.go
+++ cache.go
@@ -3,21 +3,23 @@ package cache
 import "time"
 
 type Cache struct {
+	data map[string]string
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
 
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.data = append()
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 }
