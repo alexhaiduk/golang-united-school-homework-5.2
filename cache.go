42c4ba14185290b6323db620fdddd365d5c3b014
alexhaiduk <alexhaiduk82@gmail.com>
2022-05-31 16:30:08 +0300

    first solution

--- cache.go
+++ cache.go
@@ -3,23 +3,44 @@ package cache
 import "time"
 
 type Cache struct {
-	data map[string]string
+	data     map[string]string
+	datatime map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	var data = make(map[string]string, 1)
+	var datatime = make(map[string]time.Time, 1)
+	return Cache{data, datatime}
 }
 
 func (c Cache) Get(key string) (string, bool) {
-
+	if time.Now().After(c.datatime[key]) && !c.datatime[key].IsZero() {
+		delete(c.data, key)
+		delete(c.datatime, key)
+	}
+	v, ok := c.data[key]
+	return v, ok
 }
 
 func (c Cache) Put(key, value string) {
-	c.data = append()
+	c.data[key] = value
+	c.datatime[key] = time.Date(0001, 1, 1, 00, 00, 00, 00, time.UTC)
 }
 
 func (c Cache) Keys() []string {
+	var keys = make([]string, 1)
+	for k, _ := range c.data {
+		if time.Now().Before(c.datatime[k]) || c.datatime[k].IsZero() {
+			keys = append(keys, k)
+		} else if time.Now().After(c.datatime[k]) && !c.datatime[k].IsZero() {
+			delete(c.data, k)
+			delete(c.datatime, k)
+		}
+	}
+	return keys
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = value
+	c.datatime[key] = deadline
 }
